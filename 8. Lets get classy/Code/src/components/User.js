5b2f679be6b8819c044649fabc8a1e1917eeba59
Shreya Manna <shreya.kolkata07@gmail.com>
2024-08-05 01:03:17 +0530

    E8_Part4_State in class component

--- 8. Lets get classy/Code/src/components/User.js
+++ 8. Lets get classy/Code/src/components/User.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {useState} from 'react'
 
 const User = ({name}) =>{
+
+    const [count]= useState(0);
+    const [count2, setCount]= useState(1);
+
     return(
         <div className='user-card'>
             <p>Data from functional component</p>
+            <h5>Count = {count}</h5>
+            <h5>Count2: {count2}</h5>
             <h2>Name: {name}</h2>
             <h3>Location: Dehradun</h3>
             <h4>Contact: @akshaymarch7</h4>
--- 8. Lets get classy/Code/src/components/UserClass.js
+++ 8. Lets get classy/Code/src/components/UserClass.js
@@ -5,11 +5,18 @@ class UserClass extends React.Component {
     constructor(props){
         super(props);
         console.log(props)
+        this.state = {
+            count: 0,
+            count2: 2,
+        }
     }
+
     // render(){
     //     return (
     //         <div className='user-card'>
     //             <p>Data from class component</p>
+    //             <p>Count: {this.state.count}</p>
+    //             <p>Count2: {this.state.count2}</p>
     //             <h2>Name: {this.props.name}</h2>
     //             <h3>Location: {this.props.location}</h3>
     //             <h4>Contact: @akshaymarch7</h4>
@@ -21,9 +28,21 @@ class UserClass extends React.Component {
     // With destructuring the above code can be written as below
     render(){
         const {name, location} = this.props
+        const {count, count2} = this.state
+
         return(
             <div className='user-card'>
                 <p>Data from class component</p>
+                <h3>Count: {count}</h3>
+                <button onClick={()=>{
+                    // NEVER UPDATE STATE VARIABLES DIRECTLY AS SHOWN BELOW
+                    // this.state.count= this.state.count + 1
+                    this.setState({
+                        count: this.state.count + 1
+                        // We can update other state variable here as well
+                    })
+                }}>Count Increase</button>
+                <p>Count2: {count2}</p>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
             </div>
